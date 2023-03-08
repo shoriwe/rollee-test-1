0fa8b9ab140111d06cf09845824348d4ab7e4d3a
Antonio <antoniojosedonishung@gmail.com>
2023-03-08 10:21:20 -0500

    feat: solution to the test 1

--- rollee.go
+++ rollee.go
@@ -1,5 +1,7 @@
 package rollee
 
+import "sync"
+
 type ID = int
 
 // We suppose L is always valid with len (l.Values) >= 1).
@@ -9,13 +11,48 @@ type List struct {
 }
 
 func Fold(initialValue int, f func(int, int) int, l List) map[ID]int {
-	panic("not implemented")
+	accumulator := initialValue
+	for _, value := range l.Values {
+		accumulator = f(accumulator, value)
+	}
+	return map[ID]int{l.ID: accumulator}
 }
 
 func FoldChan(initialValue int, f func(int, int) int, ch chan List) map[ID]int {
-	panic("not implemented")
+	result := map[ID]int{}
+	for list := range ch {
+		if _, found := result[list.ID]; !found {
+			result[list.ID] = initialValue
+		}
+		for _, value := range list.Values {
+			result[list.ID] = f(result[list.ID], value)
+		}
+	}
+	return result
 }
 
 func FoldChanX(initialValue int, f func(int, int) int, chs ...chan List) map[ID]int {
-	panic("not implemented")
+	var (
+		result      = map[ID]int{}
+		resultMutex sync.Mutex
+		wg          sync.WaitGroup
+	)
+	wg.Add(len(chs))
+	for _, ch := range chs {
+		go func(channel chan List) {
+			defer wg.Done()
+			localResult := FoldChan(initialValue, f, channel)
+			resultMutex.Lock()
+			defer resultMutex.Unlock()
+			for id, value := range localResult {
+				if current, found := result[id]; found {
+					result[id] = f(current, value)
+				} else {
+					result[id] = value
+				}
+			}
+		}(ch)
+	}
+	wg.Wait()
+	return result
 }
